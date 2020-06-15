93ca9838f15b7a60aebb2ff47c19d43b69cf8822
muraliawspractice <muraliawspractice@gmail.com>
2020-06-15 15:10:16 +0530

    added the files

---
+++ sample54restAPI/getEmpDetails/src/main/java/com/example/getEmpDetails/GetEmpDetailsApplication.java
@@ -0,0 +1,13 @@
+package com.example.getEmpDetails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class GetEmpDetailsApplication {
+
+	public static void main(String[] args) {
+        SpringApplication.run(GetEmpDetailsApplication.class, args);
+    }
+}
---
+++ sample54restAPI/getEmpDetails/src/test/java/com/example/getEmpDetails/GetEmpDetailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.getEmpDetails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetEmpDetailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
